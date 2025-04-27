06f66463274f21f73dc2098ad790be449cd72fcc
anoyKhan <anoyk49@gmail.com>
2025-04-27 23:18:47 +0600

    Added problem K -with explanation

---
+++ 800/K_Max_And_Min.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c,min,max;
+    cin>>a>>b>>c;
+    if(a>b && a>c){
+        max = a;
+    }else if(b>a && b>c){
+        max = b;
+    }else{
+        max = c;
+    }if(a<b && a<c){
+        min = a;
+    }else if(b<a && b<c){
+        min = b;
+    }else{
+        min = c;
+    }
+    cout<<min<<" "<<max;
+    
+    return 0;
+}
