2b0616a8f4398f7f9793dbd25b6498bafc316dcf
anoyKhan <anoykhan14@gmail.com>
2025-05-09 23:11:35 +0600

    Added problem V -with explanation

---
+++ 800/Sheet_1(Data type - Conditions)/Solution/V_Comparison.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int a,b;
+    char s;
+    cin>>a>>s>>b;
+    if(s == '>'){
+        if(a>b){
+            cout<<"Right";
+        }else{
+            cout<<"Wrong";
+        }
+    }else if(s == '<'){
+        if(a<b){
+            cout<<"Right";
+        }else{
+            cout<<"Wrong";
+        }
+    }else if(s == '='){
+        if(a==b){
+            cout<<"Right";
+        }else{
+            cout<<"Wrong";
+        }
+    }
+    return 0;
+}
