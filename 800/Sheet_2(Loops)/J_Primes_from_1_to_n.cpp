bbcf3bdf5f1ae2cc5f322efce7e8c69c1c84813c
AnoyKhan <anoykhan14@gmail.com>
2025-10-21 18:49:15 +0600

    Added Sheet 2(Loops) J Primes from 1 to n

---
+++ 800/Sheet_2(Loops)/J_Primes_from_1_to_n.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    if (n == 2) return true;
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+int main() {
+    int n;
+    cin >> n;
+    for(int i=0; i<=n; i++){
+        if (isPrime(i))
+            cout << i << " ";
+    }
+    return 0;
+}
